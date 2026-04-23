0b77976ad7f597f198854e8d8885190710226e02
ishaaq08 <ishaaq001@gmail.com>
2026-04-23 23:39:36 +0100

    What is this change

--- producer/src/main/java/com/ishaaq/app/BrokerConfig.java
+++ producer/src/main/java/com/ishaaq/app/BrokerConfig.java
@@ -1,4 +1,23 @@
 package com.ishaaq.app;
 
-public class Configuration {
+public class BrokerConfig {
+    private String brokerAddress;
+
+    public BrokerConfig() {
+        String brokerAddressEnv = System.getenv("BROKER_ADDRESS");
+        if (brokerAddressEnv != null) {
+            System.out.println("--> Successfully retrieved environment variable BROKER_ADDRESS");
+            System.out.println("--> access via the instance variable brokerAddress");
+            this.brokerAddress = brokerAddressEnv;
+        } else {
+            throw new RuntimeException("The environment variable BROKER_ADDRESS has not been set. This is required when " +
+                    "connecting to the Kafka cluster. Failed to creater instance of BrokerConfig. Please set the environment" +
+                    " variable then try again.");
+        }
+    }
+
+    public String getBrokerAddress() {
+        System.out.println("--> retrieving broker address: " + brokerAddress);
+        return brokerAddress;
+    };
 }
