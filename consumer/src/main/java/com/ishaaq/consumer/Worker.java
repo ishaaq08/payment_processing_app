992c15a7795c8f05b44344ad657b3cd9a1961465
ishaaq08 <ishaaq001@gmail.com>
2026-08-03 16:24:07 +0100

    Incorrectly termed a private method as a setter.

--- consumer/src/main/java/com/ishaaq/consumer/Worker.java
+++ consumer/src/main/java/com/ishaaq/consumer/Worker.java
@@ -4,7 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.common.protocol.types.Field;
 
 public class Worker {
-    private Thread workerThread = null;
+    private Thread workerThread;
     public ThreadExceptionData workerThreadExceptionData = new ThreadExceptionData();
 
     Worker (DatabaseOps dbConn, ConsumerRecords<String, String> records, int nextBatchSizeToInsert) {
@@ -17,12 +17,13 @@ public class Worker {
     }
 
     // Setter(s)
-    public void setWorkerThread(DatabaseOps dbConn, ConsumerRecords<String, String> records, int nextBatchSizeToInsert) {
+
+    // Method
+    private void setWorkerThread(DatabaseOps dbConn, ConsumerRecords<String, String> records, int nextBatchSizeToInsert) {
         Task processingTask = new Task(dbConn, records, nextBatchSizeToInsert);
         workerThread = new Thread(processingTask);
     }
 
-    // Method
     public void runWorkerThread() {
         workerThread.start();
     }
---
+++ consumer/src/main/java/com/ishaaq/consumer/WorkerExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.ishaaq.consumer;
+
+public class workerExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+
+    }
+}
